65e4316ad21e9e334a356b5786d61d07149ce8b4
Sahar Javadi <sahar.javadi.h@gmail.com>
2025-05-17 00:12:35 +0330

    move app.js to public folder

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
-import CategoryView from "./categoryView.js";
-import ProductView from "./productView.js";
+import CategoryView from "../src/js/categoryView.js";
+import ProductView from "../src/js/productView.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   CategoryView.setupCategories();
   CategoryView.createCategoriesList();
 
   ProductView.setupProducts();
+  // ProductView.createProductsList();
 });
